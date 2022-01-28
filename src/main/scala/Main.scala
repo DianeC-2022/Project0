8b344d53041dbf73f7593e751d2eb7aeabcc6921
DianeC-2022 <97700377+DianeC-2022@users.noreply.github.com>
2022-01-28 13:48:48 -0600

    First commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+import java.sql.DriverManager
+import java.sql.Connection
+import scala.util.Random
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+object Main {
+  case class handyman_employees()
+
+  def main(args: Array[String]): Unit = {
+    println("Welcome to the Handyman Services.")
+    println("Which job would you like to select?")
+
+    val results = ("SELECT * from handyman_employees.available_jobs")
+
+      }
+
+
+    }
+
